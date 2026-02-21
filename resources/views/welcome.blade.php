740e098e876fe0a30ab6510bb8a51499efdd5998
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2026-02-21 18:51:04 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Pertemuan 1 Laravel</title>
+</head>
+<body style="background:#0f172a; color:white; font-family:Arial;">
+    <div style="display:flex; justify-content:center; align-items:center; height:100vh;">
+        <div style="background:#111827; padding:40px; border-radius:12px; text-align:center; box-shadow:0 0 20px #2563eb;">
+            <h1 style="color:#38bdf8;">Tugas Laravel Pertemuan 1</h1>
+            <h2>Nama: <b>Muhammad Shidqul Iman</b></h2>
+            <p>NIM: <b>20230140233</b></p>
+
+            <button style="margin-top:15px; padding:10px 20px; background:#2563eb; color:white; border:none; border-radius:6px;">
+                Laravel Berhasil Diinstall 🚀
+            </button>
+        </div>
+    </div>
+</body>
+</html>
